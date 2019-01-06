fd474bfd6191802a9101cd0771bc3bda41689d45
thread <thread@threadbox.net>
2019-01-05 21:59:32 -0500

    frontend things

--- lib/lucidboard_web/view_helper.ex
+++ lib/lucidboard_web/view_helper.ex
@@ -3,7 +3,23 @@ defmodule LucidboardWeb.ViewHelper do
   import Phoenix.HTML, only: [raw: 1]
 
   @doc "Create a font-awesome icon by name"
-  def fa(name) do
-    raw(~s|<i class="fas fa-#{name}"></i>|)
+  def fa(name, class \\ nil) do
+    class = Enum.join(["icon", class], " ")
+
+    """
+    <span class="#{class}">
+      <i class="fas fa-#{name}"></i>
+    </span>
+    """
+    |> raw()
+  end
+
+  def show_card_count(column) do
+    count =
+      Enum.reduce(column.piles, 0, fn pile, acc ->
+        acc + length(pile.cards)
+      end)
+
+    "#{count} card" <> if count == 1, do: "", else: "s"
   end
 end
